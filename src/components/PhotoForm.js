f707a16b0ec44407fbf6e2f2fe586d8715d234b8
gondorya <monia87f@gmail.com>
2018-04-07 09:28:40 +0200

    added calendar

--- src/components/PhotoForm.js
+++ src/components/PhotoForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('DD MMM YYYY'));
 
 export default class PhotoForm extends React.Component {
     state = {
         title: '',
         description: '',
-        imgUrl: ''
+        imgUrl: '',
+        createdAt: moment(),
+        calendarFocused: false
     };
     onTitleChange = (e) => {
         const title = e.target.value;
@@ -17,6 +25,14 @@ export default class PhotoForm extends React.Component {
     onImgUrlChange = (e) => {
         const imgUrl = e.target.value;
         this.setState(() => ({imgUrl}));
+    };
+    onDateChange = (createdAt) => {
+        this.setState(() => ({createdAt}));
+    };
+    onFocusChange = ({focused}) => {
+        this.setState(() => ({
+            calendarFocused: focused
+        }));
     };
     render() {
         return (
@@ -44,11 +60,22 @@ export default class PhotoForm extends React.Component {
                         <input
                             type="text"
                             name="Image"
+                            placeholder="Image url"
                             value={this.state.imgUrl}
                             onChange={this.onImgUtlChange}
                         />
                     </p>
-                    <button className="button button--add">Add Photo</button>
+                        <SingleDatePicker
+                            date={this.state.createdAt}
+                            onDateChange={this.onDateChange}
+                            focused={this.state.calendarFocused}
+                            onFocusChange={this.onFocusChange}
+                            numberOfMonths={1}
+                            isOutsideRange={() => false}
+                        />
+                    <p>
+                        <button className="button button--add">Add Photo</button>
+                    </p>
                 </form>
             </div>
         )
